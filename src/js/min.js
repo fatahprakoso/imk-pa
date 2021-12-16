2a85e39ff938528dce51705622173f30cb6cf8db
fatahprakoso <67370171+fatahprakoso@users.noreply.github.com>
2021-12-16 22:00:22 +0700

    add: navbar generator

--- src/js/min.js
+++ src/js/min.js
@@ -2,4 +2,5 @@
 // import * as bootstrap from 'bootstrap';
 
 import { Button, Carousel, Accordion } from 'bootstrap';
+import './navbar-footer-generator'
 import './login-register-popup';
--- src/js/navbar-footer-generator.js
+++ src/js/navbar-footer-generator.js
@@ -1,1 +1,60 @@
 let page = window.location.pathname.split("/").pop();
+let pages = {
+    home: page !== 'home.html' ? './home.html' : '#',
+    artikel: page !== 'artikel.html' ? './artikel.html' : '#',
+    detail_artikel: page !== 'detail_artikel.html' ? './detail_artikel.html' : '#',
+    detail_produk: page !== 'detail_produk.html' ? './detail_produk.html' : '#',
+    tentang_perusahaan: page !== 'tentang_perusahaan.html' ? './tentang_perusahaan.html' : '#',
+    belanja: page !== 'belanja.html' ? '#' : '#',
+}
+
+console.log(page);
+
+const navbar = document.createElement('nav');
+navbar.classList.add('ph-nav-color', 'fixed-top');
+navbar.innerHTML = `
+    <div class="container-fluid mt-1 mb-1">
+        <div class="row">
+          <div class="col">
+            <div class="d-flex align-items-center justify-content-center">
+              <a class="navbar-brand ph-nav-main-text my-auto" href="${pages.home}"
+                >PasarHijau!</a
+              >
+            </div>
+          </div>
+          <div class="col-7">
+            <div class="d-flex justify-content-between align-items-center">
+              <a class="nav-link ph-nav-text-size" href="${pages.belanja}">Belanja</a>
+              <a class="nav-link ph-nav-text-size" href="${pages.artikel}"
+                >Artikel</a
+              >
+              <a class="nav-link ph-nav-text-size" href="${pages.tentang_perusahaan}"
+                >Tentang Perusahaan</a
+              >
+              <form class="d-flex mt-1 mb-1">
+                <input
+                  class="form-control me-2"
+                  type="search"
+                  placeholder="Search"
+                  aria-label="Search"
+                />
+                <button class="btn btn-outline-success" type="submit">
+                  Search
+                </button>
+              </form>
+            </div>
+          </div>
+          <div class="col d-flex justify-content-center">
+            <a
+              class="navbar-text text-center ph-nav-text-size my-auto"
+              style="text-decoration: none"
+              href="#"
+              id="login-register"
+              >Login/Register</a
+            >
+          </div>
+        </div>
+    </div>
+`
+
+document.querySelector('body').insertAdjacentElement('afterbegin', navbar);
